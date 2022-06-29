61130b7755386bf9dcd16e3b781096d79e93c753
Marcos Macedo <marcosmacedoo.dev@gmail.com>
2022-06-29 20:31:41 -0300

    test: testes unitarios para validacao de numero de telefone e email

--- src/test/unit/presentation/utils/utils.test.ts
+++ src/test/unit/presentation/utils/utils.test.ts
@@ -1,31 +1,88 @@
-import { isCpfValid } from "../../../../presentation/utils"
+import {
+    isCpfValid,
+    isEmailValid,
+    isPhoneValid,
+} from "../../../../presentation/utils";
 
-describe('Validate cpf', () => {
-    it('Test cpf equal 35801215883', () => {
-        const cpf = '35801215883'
-        const response = isCpfValid(cpf)
+describe("Validate cpf", () => {
+    it("Test cpf equal 35801215883", () => {
+        const cpf = "35801215883";
+        const response = isCpfValid(cpf);
 
-        expect(response).toEqual(true)
-    })
+        expect(response).toEqual(true);
+    });
 
-    it('Test cpf equal 3oy5883', () => {
-        const cpf = '3oy5883'
-        const response = isCpfValid(cpf)
+    it("Test cpf equal 3oy5883", () => {
+        const cpf = "3oy5883";
+        const response = isCpfValid(cpf);
 
-        expect(response).toEqual(false)
-    })
+        expect(response).toEqual(false);
+    });
 
-    it('Test cpf equal 3580-215883', () => {
-        const cpf = '3580-215883'
-        const response = isCpfValid(cpf)
+    it("Test cpf equal 3580-215883", () => {
+        const cpf = "3580-215883";
+        const response = isCpfValid(cpf);
 
-        expect(response).toEqual(false)
-    })
+        expect(response).toEqual(false);
+    });
 
     it('Test cpf equal ""', () => {
-        const cpf = ''
-        const response = isCpfValid(cpf)
+        const cpf = "";
+        const response = isCpfValid(cpf);
 
-        expect(response).toEqual(false)
-    })
-})
+        expect(response).toEqual(false);
+    });
+});
+
+describe("Validate phone", () => {
+    it("Test phone equal 86995028103", () => {
+        const phone = "86995028103";
+        const response = isPhoneValid(phone);
+
+        expect(response).toEqual(true);
+    });
+
+    it("Test phone equal 8699502810u", () => {
+        const phone = "8699502810u";
+        const response = isPhoneValid(phone);
+
+        expect(response).toEqual(false);
+    });
+
+    it("Test phone equal 8699501", () => {
+        const phone = "8699501";
+        const response = isPhoneValid(phone);
+
+        expect(response).toEqual(false);
+    });
+
+    it("Test phone equal 8699501399381", () => {
+        const phone = "8699501399381";
+        const response = isPhoneValid(phone);
+
+        expect(response).toEqual(false);
+    });
+});
+
+describe("Validate email", () => {
+    it("Test email equal marcos@gmail.com", () => {
+        const email = "marcos@gmail.com";
+        const response = isEmailValid(email);
+
+        expect(response).toEqual(true);
+    });
+
+    it("Test email equal mewdfjiwe$2", () => {
+        const email = "mewdfjiwe$2";
+        const response = isEmailValid(email);
+
+        expect(response).toEqual(false);
+    });
+
+    it("Test email equal fido.dev.cirf.br", () => {
+        const email = "fido.dev.cirf.br";
+        const response = isEmailValid(email);
+
+        expect(response).toEqual(false);
+    });
+});
